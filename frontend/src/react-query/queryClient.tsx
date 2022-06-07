938b329bfd7e18d6164a750860e513cece83f1ad
Michal Hanuliak <hanuliak@digismoothie.com>
2022-06-07 12:28:39 +0200

    Change linting rules

--- frontend/src/react-query/queryClient.tsx
+++ frontend/src/react-query/queryClient.tsx
@@ -1,9 +1,12 @@
 import { QueryClient } from 'react-query';
 
 const queryErrorHandle = (error: unknown) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const title =
     error instanceof Error ? error.message : 'Error connecting to server';
+
   // TODO: Error toast
+  return title as unknown as void;
 };
 
 export const queryClient = new QueryClient({
